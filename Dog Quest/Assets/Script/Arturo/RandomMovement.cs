229b74517e432413ed6921e5e1fc62d872f962d8
CamiloArt <camilocastro316@gmail.com>
2017-02-23 20:28:55 -0500

    Random movement script

---
+++ Dog Quest/Assets/Script/Arturo/RandomMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour {
+
+	public float speed;
+	public float maxPathLegth;
+	public float wallAvoidDistance;
+	public bool reachpoint;
+	public Vector3 target = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		SetPoint ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform.position = Random.insideUnitCircle;
+
+		this.transform.position += target * speed * Time.deltaTime;
+	}
+	void SetPoint(){
+		Vector3 tempPoint = Random.insideUnitCircle * 5;
+		target.x = tempPoint.x;
+		target.z = tempPoint.y;
+		Invoke("SetPoint",10f);
+	}
+}
