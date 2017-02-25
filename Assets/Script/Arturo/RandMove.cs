e93d2b0079aca292d5d104a6d864709169e21e34
CamiloArt <camilocastro316@gmail.com>
2017-02-25 15:10:09 -0500

    Random movement working

--- Assets/Script/Arturo/RandMove.cs
+++ Assets/Script/Arturo/RandMove.cs
@@ -9,21 +9,51 @@ public class RandMove : MonoBehaviour {
 	public float wallAvoidDistance;
 	public bool reachpoint;
 	public Vector3 target = Vector3.zero;
+	public bool ChangeDir = false;
+	public bool coliding;
+	private CharacterController controller;
+	public float timeCounter = 0;
+	public float WalkingTime;
+	public float IdleTime;
 	// Use this for initialization
 	void Start () {
 		SetPoint ();
+		controller = this.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//this.transform.position = Random.insideUnitCircle;
+		timeCounter += Time.deltaTime;
+		if (timeCounter > WalkingTime + IdleTime) {
+			SetPoint ();
+			timeCounter = 0;
+		}
+		if (ChangeDir) {
+			SetPoint ();
+			ChangeDir = false;
+		}
+		if(timeCounter < WalkingTime){
+			controller.Move (target * speed * Time.deltaTime);
+		}
+		//if (Physics.Raycast (this.transform.position, target, 2.5f)) {
+		//	ChangeDir = true;
+		//}
+		//Debug.DrawRay(this.transform.position,target*2.5f);
 
-		this.transform.position += target * speed * Time.deltaTime;
+	}
+	void OnCollisionEnter(Collision collision) {
+		ChangeDir = true;
+		Debug.Log ("collision");
+	}
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		ChangeDir = true;
+		Debug.Log ("collision");
 	}
 	void SetPoint(){
-		Vector3 tempPoint = Random.insideUnitCircle * 5;
+		Vector3 tempPoint = Random.insideUnitCircle;
 		target.x = tempPoint.x;
 		target.z = tempPoint.y;
-		Invoke("SetPoint",10f);
+		target = target.normalized;
 	}
 }
